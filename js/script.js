d387d1f0fbfb63c7d09bb54d9f2e9553d3db23fc
Gianpaolo <pao.barga@gmail.com>
2020-12-17 16:06:42 +0100

    aggiunta versione console.log leggermenete diversa

--- js/script.js
+++ js/script.js
@@ -1,16 +1,32 @@
-// creare num da 1 a 100
+// // versione 1
+// // creare num da 1 a 100
+// for (var i = 1; i <= 100; i++) {
+//     // se divisibili per entrambi, stampa FizzBuzz
+//     if (i % 3 === 0 && i % 5 === 0) {
+//         console.log('FizzBuzz');
+//     // se divisibili per 3, stampa Buzz
+//     } else if (i % 3 === 0){
+//         console.log('Fizz');
+//     // se divisibili per 5, stampa Fizz
+//     } else if (i % 5 === 0) {
+//         console.log('Buzz')
+//     // altrimenti stampa il numero stesso
+//     } else {
+//         console.log(i)
+//     }
+// }
+
+// versione 2
 for (var i = 1; i <= 100; i++) {
-    // se divisibili per entrambi, stampa FizzBuzz
     if (i % 3 === 0 && i % 5 === 0) {
-        console.log('FizzBuzz');
-    // se divisibili per 3, stampa Buzz
-    } else if (i % 3 === 0){
-        console.log('Fizz');
-    // se divisibili per 5, stampa Fizz
-    } else if (i % 5 === 0) {
-        console.log('Buzz')
-    // altrimenti stampa il numero stesso
+        console.log('FizzBuzz')
     } else {
-        console.log(i)
+        if (i % 3 === 0) {
+            console.log('Fizz');
+        } else if (i % 5 === 0) {
+            console.log('Buzz');
+        } else {
+            console.log(i);
+        }
     }
 }
